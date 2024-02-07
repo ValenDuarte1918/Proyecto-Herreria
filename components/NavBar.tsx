06e1051701fadd45d3e7af6594570e20af8b08ba
valenduarte1918 <valentinduarte1@hotmail.com>
2024-02-07 19:43:41 -0300

    arreglo en el navBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,60 +1,44 @@
 import type { NextPage } from "next";
-import { memo } from "react";
-
-const NavBar: NextPage = memo(() => {
-  return (
-    <header className="self-stretch h-[114px] bg-white shadow-[0px_3px_11px_rgba(0,_0,_0,_0.25)] flex flex-row items-start justify-between pt-[3px] pb-0 pr-0 pl-[75px] box-border gap-[20px] top-[0] z-[99] sticky max-w-full text-left text-5xl text-darkslategray-200 font-segoe-ui mq825:pl-[37px] mq825:box-border">
-      <img
-        className="h-[105px] w-[283px] relative"
-        loading="eager"
-        alt=""
-        src="/div-logo.svg"
-      />
-      <div className="self-stretch w-[764px] [filter:drop-shadow(0px_4px_4px_rgba(0,_0,_0,_0.25)_inset)] flex flex-row items-center justify-start py-[27px] px-[23px] box-border max-w-full">
-        <div className="h-[29px] flex flex-col items-start justify-start pt-1.5 pb-0 pr-[11px] pl-0 box-border text-midnightblue">
-          <div className="flex-1 flex flex-col items-start justify-start gap-[3px]">
-            <div className="flex-1 relative tracking-[0.45px] leading-[22.5px] flex items-center">
-              INICIO
-            </div>
-            <div className="h-0 flex flex-row items-start justify-start py-0 px-[3px] box-border">
-              <img
-                className="h-0 w-[54px] relative object-contain"
-                loading="eager"
-                alt=""
-                src="/navigation--menubar--menuitem--link.svg"
-              />
-            </div>
+import React from "react";
+  
+const NavBar = ({ className }: { className?: string }) => {
+      return (
+          <div className={`flex flex-col w-[1507px] h-[120px] items-end justify-center gap px-12 py-[3px] relative bg-white shadow-[0px_3px_11px_#00000040] ${className}`}>
+              <div className="relative w-[847px] h-[1120px] mt-[-1.50px] mb-[-1.50px] z-[1]">
+                  <div className="flex w-[607px] h-[45px] items-baseline gap-[3px] px-0 py-[31px] relative top-[6px] left-[156px] ">
+                      <img
+                          className="absolute w-[45px] h-[25px] mt 10px top-[50px] left-[25px]"
+                          alt="Navigation menubar"
+                          src="navigation--menubar--menuitem--link.svg"
+                      />
+                      <div className=" relative w-[73px] h-[19px] z-[6] [font-family:'Segoe_UI-Semilight',Helvetica] font-normal text-[#0c2075] text-[16px] tracking-[0.45px] leading-[22px] relative text-center whitespace-nowrap">
+                          &nbsp;&nbsp;INICIO
+                      </div>
+                      <div className="relative w-[5px] h-[18px] z-[3] [font-family:'Roboto-Bold',Helvetica] font-bold text-[#203b1b] text-[24px] text-center tracking-[0] leading-[22.5px] whitespace-nowrap">
+                          |
+                      </div>
+                      <div className="relative w-[162px] h-[36px] mt-[-1.00px] z-[4] [font-family:'Segoe_UI-Semilight',Helvetica] font-normal text-[#3b3b3b] text-[16px] text-center tracking-[0.45px] leading-[22.5px]">
+                          &nbsp;&nbsp;QUIÉNES SOMOS
+                      </div>
+                      <div className="relative w-[5px] h-[18px] z-[3] [font-family:'Roboto-Bold',Helvetica] font-bold text-[#203b1b] text-[24px] text-center tracking-[0] leading-[22.5px] whitespace-nowrap">
+                          |
+                      </div>
+                      <div className="relative w-[162px] h-[18px] z-[2] [font-family:'Segoe_UI-Semilight',Helvetica] font-normal text-[#3b3b3b] text-[16px] text-center tracking-[0.45px] leading-[22.5px] whitespace-nowrap">
+                          &nbsp;&nbsp; QUÉ HACEMOS
+                      </div>
+                      <div className="relative w-[5px] h-[18px] z-[1] [font-family:'Roboto-Bold',Helvetica] font-bold text-[#203b1b] text-[24px] text-center tracking-[0] leading-[22.5px] whitespace-nowrap">
+                          |
+                      </div>
+                      <div className="relative w-[93px] z-0 [font-family:'Segoe_UI-Semilight',Helvetica] font-normal text-[#3b3b3b] text-[16px] text-center tracking-[0.45px] leading-[22.5px]">
+                          &nbsp;&nbsp;CONTACTO
+                      </div>
+                  </div>
+              </div>
+              <div className="inline-flex flex-col h-[105px] items-center justify-around gap-[10px] absolute top-[3px] left-[82px] z-0">
+                  <img className="relative w-[268.28px] h-[98.96px]" alt="Group" src="div-logo.svg" />
+              </div>
           </div>
-        </div>
-        <div className="h-[22px] w-4 flex flex-col items-start justify-start pt-1 px-0 pb-0 box-border font-roboto">
-          <b className="flex-1 relative leading-[22.5px] flex items-center [transform:_rotate(180deg)]">
-            |
-          </b>
-        </div>
-        <div className="w-[198px] flex flex-col items-start justify-start py-0 px-0 box-border text-darkslategray-100">
-          <div className="self-stretch h-9 relative tracking-[0.45px] leading-[22.5px] flex items-center shrink-0 whitespace-nowrap">
-            QUIÉNES SOMOS
-          </div>
-        </div>
-        <div className="h-5 w-[207px] relative font-roboto">
-          <div className="absolute top-[0px] left-[13px] tracking-[0.45px] leading-[22.5px] font-segoe-ui text-darkslategray-100 flex items-center w-[173px] h-[18px] whitespace-nowrap">
-            QUÉ HACEMOS
-          </div>
-          <b className="absolute top-[1px] left-[194px] leading-[22.5px] flex items-center w-[5px] h-[18px]">
-            |
-          </b>
-          <b className="absolute top-[2px] left-[0px] leading-[22.5px] flex items-center w-[5px] h-[18px]">
-            |
-          </b>
-        </div>
-        <div className="h-[22px] w-[134px] flex flex-col items-start justify-start pt-0 px-0 pb-[5px] box-border text-darkslategray-100">
-          <div className="self-stretch flex-1 relative tracking-[0.45px] leading-[22.5px] flex items-center">
-            CONTACTO
-          </div>
-        </div>
-      </div>
-    </header>
-  );
-});
-
+      );
+  };
+  
 export default NavBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
     extend: {
       colors: {
         gainsboro: "#d7dacf",
-        gray: "rgba(0, 0, 0, 0.5)",
         white: "#fff",
+        gray: "rgba(0, 0, 0, 0.5)",
         darkslategray: {
           "100": "#3b3b3b",
           "200": "#203b1b",
@@ -23,19 +23,11 @@ module.exports = {
       },
     },
     fontSize: {
+      xl: "20px",
+      "37xl": "56px",
+      base: "16px",
       "5xl": "24px",
       inherit: "inherit",
-    },
-    screens: {
-      mq825: {
-        raw: "screen and (max-width: 825px)",
-      },
-      mq675: {
-        raw: "screen and (max-width: 675px)",
-      },
-      mq450: {
-        raw: "screen and (max-width: 450px)",
-      },
     },
   },
   corePlugins: {
